710b3b9c2c8ded6551cfcdfddc6ea690e2f64884
jonesmbindyo <jonesmbindyo@gmail.com>
2025-10-15 23:19:14 +0300

    Add weatherMain property to CurrentWeatherEntity and ForecastEntity

--- WeatherApp/data/src/main/java/com/jones/data/local/database/WeatherDatabase.kt
+++ WeatherApp/data/src/main/java/com/jones/data/local/database/WeatherDatabase.kt
@@ -9,7 +9,7 @@ import com.jones.data.local.entity.ForecastEntity
 
 @Database(
     entities = [CurrentWeatherEntity::class, ForecastEntity::class],
-    version = 2,
+    version = 3,
     exportSchema = false,
 )
 abstract class WeatherDatabase : RoomDatabase() {
